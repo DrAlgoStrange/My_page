542b866140220586694ce62b22a920692564636e
suryahj <94169519+suryahj@users.noreply.github.com>
2025-06-14 14:29:26 +0530

    Setting of base path to my page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my_page1/', // <-- your repo name here!
+  base: '/My_page/', // <-- your repo name here!
   plugins: [react()],
 });
